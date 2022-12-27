7372d0450d3d02cefef34f53441ceba167b91f16
cabrit0 <cabrit0o.dev@gmail.com>
2022-12-27 23:51:52 +0000

    updatedOrigins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
   "http://localhost:3000",
   "https://fitness-api.onrender.com",
-  "https://fitness4u-m48m.onrender.com",
+  "https://fitness4u-n7f4.onrender.com",
   "https://www.google.com",
 ]; // colocar url do frontend quando deploy
 
